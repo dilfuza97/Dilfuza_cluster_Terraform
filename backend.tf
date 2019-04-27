1c1f85cb060f12b1d30abe912e2e1897f6b73aa9
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-27 17:51:26 -0500

    terraform buckend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
+  required_version = ">= 0.9.3"
   backend "s3" {
-    bucket = "dilfuzacluster.com"
+    bucket = "dilfuzacluster.com" 
     key = "state/Dilfuza_cluster_Terraform"
     region = "eu-west-1"
   }

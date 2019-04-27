02381dc4ef152fe262190eb89975d0adc4449d0c
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-27 16:07:25 -0500

    fixed error

--- igw.tf
+++ igw.tf
@@ -1,9 +1,9 @@
-resource "aws_internet_gateway" "farrukhcluster-com" {
-  vpc_id = "${aws_vpc.farrukhcluster-com.id}"
+resource "aws_internet_gateway" "dilfuzacluster-com" {
+  vpc_id = "${aws_vpc.dilfuzacluster-com.id}"
 
   tags = {
-    KubernetesCluster                          = "farrukhcluster.com"
-    Name                                       = "farrukhcluster.com"
-    "kubernetes.io/cluster/farrukhcluster.com" = "owned"
+    KubernetesCluster                          = "dilfuzacluster.com"
+    Name                                       = "dilfuzacluster.com"
+    "kubernetes.io/cluster/dilfuzacluster.com" = "owned"
   }
 }

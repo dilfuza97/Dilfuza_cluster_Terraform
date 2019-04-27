d410f3ae629da760c7b8dba7da3f347c5e67d483
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-04-27 16:51:06 -0500

    Update route_table.tf

--- route_table.tf
+++ route_table.tf
@@ -1,99 +1,99 @@
 resource "aws_route" "0-0-0-0--0" {
-  route_table_id         = "${aws_route_table.farrukhcluster-com.id}"
+  route_table_id         = "${aws_route_table.dilfuzacluster-com.id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.farrukhcluster-com.id}"
+  gateway_id             = "${aws_internet_gateway.dilfuzacluster-com.id}"
 }
 
 resource "aws_route" "private-eu-west-1a-0-0-0-0--0" {
-  route_table_id         = "${aws_route_table.private-eu-west-1a-farrukhcluster-com.id}"
+  route_table_id         = "${aws_route_table.private-eu-west-1a-dilfuzacluster-com.id}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.eu-west-1a-farrukhcluster-com.id}"
+  nat_gateway_id         = "${aws_nat_gateway.eu-west-1a-dilfuzacluster-com.id}"
 }
 
 resource "aws_route" "private-eu-west-1b-0-0-0-0--0" {
-  route_table_id         = "${aws_route_table.private-eu-west-1b-farrukhcluster-com.id}"
+  route_table_id         = "${aws_route_table.private-eu-west-1b-dilfuzacluster-com.id}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.eu-west-1b-farrukhcluster-com.id}"
+  nat_gateway_id         = "${aws_nat_gateway.eu-west-1b-dilfuzacluster-com.id}"
 }
 
 resource "aws_route" "private-eu-west-1c-0-0-0-0--0" {
-  route_table_id         = "${aws_route_table.private-eu-west-1c-farrukhcluster-com.id}"
+  route_table_id         = "${aws_route_table.private-eu-west-1c-dilfuzacluster-com.id}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.eu-west-1c-farrukhcluster-com.id}"
+  nat_gateway_id         = "${aws_nat_gateway.eu-west-1c-dilfuzacluster-com.id}"
 }
 
 
 
-resource "aws_route_table" "farrukhcluster-com" {
-  vpc_id = "${aws_vpc.farrukhcluster-com.id}"
+resource "aws_route_table" "dilfuzacluster-com" {
+  vpc_id = "${aws_vpc.dilfuzacluster-com.id}"
 
   tags = {
-    KubernetesCluster                          = "farrukhcluster.com"
-    Name                                       = "farrukhcluster.com"
-    "kubernetes.io/cluster/farrukhcluster.com" = "owned"
+    KubernetesCluster                          = "dilfuzacluster.com"
+    Name                                       = "dilfuzacluster.com"
+    "kubernetes.io/cluster/dilfuzacluster.com" = "owned"
     "kubernetes.io/kops/role"                  = "public"
   }
 }
 
-resource "aws_route_table" "private-eu-west-1a-farrukhcluster-com" {
-  vpc_id = "${aws_vpc.farrukhcluster-com.id}"
+resource "aws_route_table" "private-eu-west-1a-dilfuzacluster-com" {
+  vpc_id = "${aws_vpc.dilfuzacluster-com.id}"
 
   tags = {
-    KubernetesCluster                          = "farrukhcluster.com"
-    Name                                       = "private-eu-west-1a.farrukhcluster.com"
-    "kubernetes.io/cluster/farrukhcluster.com" = "owned"
+    KubernetesCluster                          = "dilfuzacluster.com"
+    Name                                       = "private-eu-west-1a.dilfuzacluster.com"
+    "kubernetes.io/cluster/dilfuzacluster.com" = "owned"
     "kubernetes.io/kops/role"                  = "private-eu-west-1a"
   }
 }
 
-resource "aws_route_table" "private-eu-west-1b-farrukhcluster-com" {
-  vpc_id = "${aws_vpc.farrukhcluster-com.id}"
+resource "aws_route_table" "private-eu-west-1b-dilfuzacluster-com" {
+  vpc_id = "${aws_vpc.dilfuzacluster-com.id}"
 
   tags = {
-    KubernetesCluster                          = "farrukhcluster.com"
-    Name                                       = "private-eu-west-1b.farrukhcluster.com"
-    "kubernetes.io/cluster/farrukhcluster.com" = "owned"
+    KubernetesCluster                          = "dilfuzacluster.com"
+    Name                                       = "private-eu-west-1b.dilfuzacluster.com"
+    "kubernetes.io/cluster/dilfuzacluster.com" = "owned"
     "kubernetes.io/kops/role"                  = "private-eu-west-1b"
   }
 }
 
-resource "aws_route_table" "private-eu-west-1c-farrukhcluster-com" {
-  vpc_id = "${aws_vpc.farrukhcluster-com.id}"
+resource "aws_route_table" "private-eu-west-1c-dilfuzacluster-com" {
+  vpc_id = "${aws_vpc.dilfuzacluster-com.id}"
 
   tags = {
-    KubernetesCluster                          = "farrukhcluster.com"
-    Name                                       = "private-eu-west-1c.farrukhcluster.com"
-    "kubernetes.io/cluster/farrukhcluster.com" = "owned"
+    KubernetesCluster                          = "dilfuzacluster.com"
+    Name                                       = "private-eu-west-1c.dilfuzacluster.com"
+    "kubernetes.io/cluster/dilfuzacluster.com" = "owned"
     "kubernetes.io/kops/role"                  = "private-eu-west-1c"
   }
 }
 
-resource "aws_route_table_association" "private-eu-west-1a-farrukhcluster-com" {
-  subnet_id      = "${aws_subnet.eu-west-1a-farrukhcluster-com.id}"
-  route_table_id = "${aws_route_table.private-eu-west-1a-farrukhcluster-com.id}"
+resource "aws_route_table_association" "private-eu-west-1a-dilfuzacluster-com" {
+  subnet_id      = "${aws_subnet.eu-west-1a-dilfuzacluster-com.id}"
+  route_table_id = "${aws_route_table.private-eu-west-1a-dilfuzacluster-com.id}"
 }
 
-resource "aws_route_table_association" "private-eu-west-1b-farrukhcluster-com" {
-  subnet_id      = "${aws_subnet.eu-west-1b-farrukhcluster-com.id}"
-  route_table_id = "${aws_route_table.private-eu-west-1b-farrukhcluster-com.id}"
+resource "aws_route_table_association" "private-eu-west-1b-dilfuzacluster-com" {
+  subnet_id      = "${aws_subnet.eu-west-1b-dilfuzacluster-com.id}"
+  route_table_id = "${aws_route_table.private-eu-west-1b-dilfuzacluster-com.id}"
 }
 
-resource "aws_route_table_association" "private-eu-west-1c-farrukhcluster-com" {
-  subnet_id      = "${aws_subnet.eu-west-1c-farrukhcluster-com.id}"
-  route_table_id = "${aws_route_table.private-eu-west-1c-farrukhcluster-com.id}"
+resource "aws_route_table_association" "private-eu-west-1c-dilfuzacluster-com" {
+  subnet_id      = "${aws_subnet.eu-west-1c-dilfuzacluster-com.id}"
+  route_table_id = "${aws_route_table.private-eu-west-1c-dilfuzacluster-com.id}"
 }
 
-resource "aws_route_table_association" "utility-eu-west-1a-farrukhcluster-com" {
-  subnet_id      = "${aws_subnet.utility-eu-west-1a-farrukhcluster-com.id}"
-  route_table_id = "${aws_route_table.farrukhcluster-com.id}"
+resource "aws_route_table_association" "utility-eu-west-1a-dilfuzacluster-com" {
+  subnet_id      = "${aws_subnet.utility-eu-west-1a-dilfuzacluster-com.id}"
+  route_table_id = "${aws_route_table.dilfuzacluster-com.id}"
 }
 
-resource "aws_route_table_association" "utility-eu-west-1b-farrukhcluster-com" {
-  subnet_id      = "${aws_subnet.utility-eu-west-1b-farrukhcluster-com.id}"
-  route_table_id = "${aws_route_table.farrukhcluster-com.id}"
+resource "aws_route_table_association" "utility-eu-west-1b-dilfuzacluster-com" {
+  subnet_id      = "${aws_subnet.utility-eu-west-1b-dilfuzacluster-com.id}"
+  route_table_id = "${aws_route_table.dilfuzacluster-com.id}"
 }
 
-resource "aws_route_table_association" "utility-eu-west-1c-farrukhcluster-com" {
-  subnet_id      = "${aws_subnet.utility-eu-west-1c-farrukhcluster-com.id}"
-  route_table_id = "${aws_route_table.farrukhcluster-com.id}"
+resource "aws_route_table_association" "utility-eu-west-1c-dilfuzacluster-com" {
+  subnet_id      = "${aws_subnet.utility-eu-west-1c-dilfuzacluster-com.id}"
+  route_table_id = "${aws_route_table.dilfuzacluster-com.id}"
 }

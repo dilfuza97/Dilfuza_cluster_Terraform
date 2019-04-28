059ba000fa28bed7a18958ff386541b66b95a805
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-04-27 19:14:58 -0500

    changed

--- variables.tf
+++ variables.tf
@@ -2,7 +2,14 @@ variable "bastion_min_size" {}
 variable "bastion_max_size" {}
 variable "node_min_size" {}
 variable "node_max_size" {}
-  
+variable "node_instance_type" {}
+variable "Env" {}
+variable "Created_by" {}
+variable "Dept" {}
+variable "master_instance_type" {}
+variable "image_id" {}
+variable "aws_region" {}
+variable "bastion_instance_type" {}
 
 
   
